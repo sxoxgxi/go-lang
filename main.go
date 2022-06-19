1cab4ba885821914b986f0a1c66864693c630f09
sxoxgxi <nirmalrawal198@gmail.com>
2022-06-19 20:30:00 +0545

    bitshifting

--- main.go
+++ main.go
@@ -5,11 +5,10 @@ import (
 )
 
 func main() {
-	// operators
-	a := 10             // 1010
-	b := 3              // 0011
-	fmt.Println(a & b)  // 0010
-	fmt.Println(a | b)  // 1011
-	fmt.Println(a ^ b)  // 1001
-	fmt.Println(a &^ b) // 0100
+	// bit shifting
+	a := 8 // 2^3
+	// left
+	fmt.Println(a << 3) // 2^3 * 2^3 = 2^6
+	// right
+	fmt.Println(a >> 3) // 2^3 / 2^3 = 2^0
 }
